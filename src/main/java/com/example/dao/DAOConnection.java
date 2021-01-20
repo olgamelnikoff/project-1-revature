f2e7e702ed2abef5a8500b37bb3bfd69f5badf23
Olga Melnikova <olgamelnikoff@gmail.com>
2021-01-20 16:17:26 -0500

    Added screenshots for Readme

--- src/main/java/com/example/dao/DAOConnection.java
+++ src/main/java/com/example/dao/DAOConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DAOConnection {
 	
 	private static String url = "jdbc:mariadb://revature-db.c1jnfhxqwao5.us-east-1.rds.amazonaws.com:3306/Project1";
-	private static String username = "project1user";
+	private static String username = "olgamelnikoff";
 	private static String password = "mypassword";
 	
 	public DAOConnection() {
